d69df4c2fed3dc9939d8ec132fb77f6db74745bc
Sascha Homeier <sascha.homeier@gmail.com>
2017-05-27 11:29:57 +0700

    Initial Commit

---
+++ sho.cmis.drive/src/sho/cmis/drive/CmisDrive.java
@@ -0,0 +1,240 @@
+package sho.cmis.drive;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.liferay.nativity.control.MessageListener;
+import com.liferay.nativity.control.NativityControl;
+import com.liferay.nativity.control.NativityControlUtil;
+import com.liferay.nativity.control.NativityMessage;
+import com.liferay.nativity.listeners.SocketCloseListener;
+import com.liferay.nativity.listeners.SocketOpenListener;
+import com.liferay.nativity.modules.contextmenu.ContextMenuControl;
+import com.liferay.nativity.modules.contextmenu.ContextMenuControlCallback;
+import com.liferay.nativity.modules.contextmenu.ContextMenuControlUtil;
+import com.liferay.nativity.modules.contextmenu.model.ContextMenuAction;
+import com.liferay.nativity.modules.contextmenu.model.ContextMenuItem;
+import com.liferay.nativity.modules.fileicon.FileIconControl;
+import com.liferay.nativity.modules.fileicon.FileIconControlCallback;
+import com.liferay.nativity.modules.fileicon.FileIconControlUtil;
+
+@Component(immediate = true)
+public class CmisDrive
+{
+	private static final Logger LOG = LoggerFactory.getLogger(CmisDrive.class.getName());
+
+	private static final String COMPONENT_NAME = "sho.cmis.drive";
+
+	@Activate
+	public void activate()
+	{
+		LOG.info("Activating component: {} ...", COMPONENT_NAME);
+		try
+		{
+			Thread.sleep(1000);
+		}
+		catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		nativity();
+		LOG.debug("Activated component: {}", COMPONENT_NAME);
+	}
+
+	private void nativity()
+	{
+		final String testFolder = "/Volumes/Shorty_JetDrive_1/tmp";
+
+		try
+		{
+			final NativityControl nativityControl = NativityControlUtil.getNativityControl();
+
+			nativityControl.addSocketOpenListener(new SocketOpenListener()
+			{
+
+				@Override
+				public void onSocketOpen()
+				{
+					// TODO Auto-generated method stub
+					System.out.println("Socket opened !!!!");
+
+					// nativityControl.addFavoritesPath(testFolder);
+
+					// Setting filter folders is required for Mac's Finder Sync plugin
+					nativityControl.setFilterFolder(testFolder);
+
+					final int testIconId = 98786;
+
+					// FileIconControlCallback used by Windows and Mac
+					FileIconControlCallback fileIconControlCallback = new FileIconControlCallback()
+					{
+						@Override
+						public int getIconForFile(String path)
+						{
+							// return testIconId;
+							System.out.println("Get ICon For File: " + path);
+							return testIconId;
+						}
+					};
+
+					FileIconControl fileIconControl =
+						FileIconControlUtil.getFileIconControl(nativityControl, fileIconControlCallback);
+
+					fileIconControl.enableFileIcons();
+
+					String testFilePath = testFolder + "/squirrel.zip";
+
+					// if (OSDetector.isWindows())
+					// {
+					// // This id is determined when building the DLL
+					// testIconId = 1;
+					// }
+					// else if (OSDetector.isMinimumAppleVersion(OSDetector.MAC_YOSEMITE_10_10))
+					// {
+					// Used by Mac Finder Sync. This unique id can be set at runtime.
+					// testIconId = 1;
+
+					fileIconControl.registerIconWithId(testFolder + "/omnIcon.icns", "omn", Integer.toString(testIconId));
+					// }
+					// else if (false)
+					// {
+					// // Used by Mac Injector and Linux
+					// testIconId = fileIconControl.registerIcon("/Users/liferay/Desktop/testIcon.icns");
+					// }
+				}
+			});
+
+			nativityControl.addSocketCloseListener(new SocketCloseListener()
+			{
+
+				@Override
+				public void onSocketClose()
+				{
+					// TODO Auto-generated method stub
+					System.out.println("Socket Closed .....");
+				}
+			});
+
+			nativityControl.connect();
+
+			nativityControl.addFavoritesPath(testFolder);
+
+			// Setting filter folders is required for Mac's Finder Sync plugin
+			nativityControl.setFilterFolder(testFolder);
+
+			/* File Icons */
+
+			nativityControl.registerMessageListener(new MessageListener(testFolder)
+			{
+
+				@Override
+				public NativityMessage onMessage(NativityMessage nativityMessage)
+				{
+					System.out.println("Received Message: " + nativityMessage);
+					return null;
+				}
+			});
+
+			final int testIconId = 98786;
+
+			// FileIconControlCallback used by Windows and Mac
+			FileIconControlCallback fileIconControlCallback = new FileIconControlCallback()
+			{
+				@Override
+				public int getIconForFile(String path)
+				{
+					// return testIconId;
+					System.out.println("Get ICon For File: " + path);
+					return testIconId;
+				}
+			};
+
+			FileIconControl fileIconControl = FileIconControlUtil.getFileIconControl(nativityControl, fileIconControlCallback);
+
+			fileIconControl.enableFileIcons();
+
+			String testFilePath = testFolder + "/squirrel.zip";
+
+			// if (OSDetector.isWindows())
+			// {
+			// // This id is determined when building the DLL
+			// testIconId = 1;
+			// }
+			// else if (OSDetector.isMinimumAppleVersion(OSDetector.MAC_YOSEMITE_10_10))
+			// {
+			// Used by Mac Finder Sync. This unique id can be set at runtime.
+			// testIconId = 1;
+
+			fileIconControl.registerIconWithId(testFolder + "/omnIcon.icns", "omn", Integer.toString(testIconId));
+			// }
+			// else if (false)
+			// {
+			// // Used by Mac Injector and Linux
+			// testIconId = fileIconControl.registerIcon("/Users/liferay/Desktop/testIcon.icns");
+			// }
+
+			// FileIconControl.setFileIcon() method only used by Linux
+			// fileIconControl.setFileIcon(testFilePath, testIconId);
+
+			/* Context Menus */
+
+			ContextMenuControlCallback contextMenuControlCallback = new ContextMenuControlCallback()
+			{
+				@Override
+				public List<ContextMenuItem> getContextMenuItems(String[] paths)
+				{
+					ContextMenuItem contextMenuItem = new ContextMenuItem("Nativity Test");
+
+					ContextMenuAction contextMenuAction = new ContextMenuAction()
+					{
+						@Override
+						public void onSelection(String[] paths)
+						{
+							for (String path : paths)
+							{
+								System.out.print(path + ", ");
+							}
+
+							System.out.println("selected");
+						}
+					};
+
+					contextMenuItem.setContextMenuAction(contextMenuAction);
+					contextMenuItem.setIconPath(testFolder + "/omnIcon.icns");
+
+					List<ContextMenuItem> contextMenuItems = new ArrayList<ContextMenuItem>()
+					{
+					};
+					contextMenuItems.add(contextMenuItem);
+
+					// Mac Finder Sync will only show the parent level of context menus
+					return contextMenuItems;
+				}
+			};
+
+			ContextMenuControl contextMenuControl =
+				ContextMenuControlUtil.getContextMenuControl(nativityControl, contextMenuControlCallback);
+
+			Set<String> allObservedFolders = nativityControl.getAllObservedFolders();
+			for (String folder : allObservedFolders)
+			{
+				System.out.println("OF: " + folder);
+			}
+			if (allObservedFolders.isEmpty())
+				System.out.println("OF is EMPTY!!!!");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error: " + e);
+			LOG.error("Error: ", e);
+		}
+	}
+
+}
---
+++ sho.cmis.drive/test/sho/cmis/drive/ExampleTest.java
@@ -0,0 +1,14 @@
+package sho.cmis.drive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
